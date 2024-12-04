ccff14fb1fd3ea258af5faa59e10b41cf9575c36
Antonio Pontes <anvictorsantos@gmail.com>
2024-12-04 18:29:24 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <HelloWorldComponent />
+    <LifeCycle email="meuemail@email.com" @change-image="changeImage(1)" />
+  </div>
+</template>
+
+<script>
+import HelloWorldComponent from './components/HelloWorldComponent.vue'
+import LifeCycle from './components/LifeCycle.vue'
+
+export default {
+  name: 'App',
+  components: { HelloWorldComponent, LifeCycle },
+  methods: {
+    changeImage(i) {
+      alert(`clicked ${i++} times`)
+    },
+  },
+}
+</script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>Meu nome é {{ nome }}</div>
+  <div>Meu nome email é {{ email }}</div>
+  <div>
+    <button @click="$emit('changeImage')">Mudar imagem</button>
+  </div>
+  <ul>
+    <li v-for="(item, i) in items" :key="i">{{ item }}</li>
+  </ul>
+  <br />
+  <ul>
+    <li v-for="course in courses" :key="course.id">{{ course.name }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'LifeCycle',
+  data() {
+    return {
+      nome: 'loading…',
+      items: ['1', '2', '3'],
+      courses: [
+        { id: 1, name: 'x' },
+        { id: 2, name: 'y' },
+        { id: 3, name: 'z' },
+      ],
+    }
+  },
+  props: ['email'],
+  emits: ['changeImage'],
+  created() {
+    setTimeout(() => {
+      this.nome = 'componente criado'
+    }, 1000)
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = 'component montado'
+    }, 2000)
+  },
+}
+</script>
